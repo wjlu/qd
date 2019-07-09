7510747745ff6bfec6e5e11e067cb25562ed51c8
amen <llwj.198@163.com>
2019-07-09 16:26:10 +0800

    123
    123

--- qd/spiders/qidian.py
+++ qd/spiders/qidian.py
@@ -5,7 +5,6 @@ from qd.items import UserItem
 from qd.emailSender import emailSender  # 导入发信模块
 import datetime
 
-# aaa
 class QidianSpider(Spider):
     name = 'qidian'
     allowed_domains = ['www.qidian.com']
